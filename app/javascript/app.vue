81f4bfdf3364dd786a60daac8936c85058af9b33
Carlos Rodríguez <ecarlos.rodriguez@gmail.com>
2018-05-24 18:17:28 -0500

    Vue base

---
+++ app/javascript/app.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <p @click="counter += 1">{{ message }} | {{counter}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Hello Vue!",
+      counter: 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
